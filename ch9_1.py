34abc8a4cf5dea794c0f481312372c69369fbe38
PyJabba9 <fesenko988@gmail.com>
2025-09-07 11:12:18 +0200

    Task from Claude AI, another addition to the same script. 1. Added working iteration through the lines of list, separating every line into its own dictionary and adding inside a list. 2. Added some drafts for future UI

--- ch9_1.py
+++ ch9_1.py
@@ -14,12 +14,13 @@ has to be transformed into
 """
 
 import re
-
+namebase = {'Name':'','Email':'','Phone':''}
+DB = []
 S = '''John Smith: phone 123-456-7890, email john.smith@company.com
 Maria Garcia: phone 987-654-3210, email maria.garcia@company.com
 Bob Johnson: phone 555-123-4567, email bob.johnson@company.com'''
 
-name_pattern_obj = re.compile(r'(?:(?!:).)*') # had to google this one
+name_pattern_obj = re.compile(r'(?:(?!:).)*') # had to google this one, constantly had a problem with : included in the name (then I found this solution)
 phone_pattern_num_obj = re.compile(r'\d{3}-\d{3}-\d{4}') #works - EZ one as this regex was in the book
 email_pattern_obj = re.compile(r'\w+[.]\w+@+\w+[.]+\w+') #EZ
 
@@ -35,15 +36,34 @@ nm = name_pattern_obj.search(S)
 N = S.split('\n') # splitting multiline string by newlines
 #print(N)
 
+
+
 for i in N:
 
-
-
     ph = phone_pattern_num_obj.search(i)
-    print(ph.group())
+    namebase.update({'Phone': ph.group()})
 
     eml = email_pattern_obj.search(i)
-    print(eml.group())
+    namebase.update({'Email': eml.group()})
 
     nm = name_pattern_obj.search(i)
-    print(nm.group())
+    namebase.update({'Name': nm.group()})
+
+    DB += [namebase]
+    namebase = {} # i figured out that you have to clear the dictionary after every iteration.
+#print(DB)  # it works - 3 dictionaries in one list
+
+#print whole DB  # this is print all module for future UI
+#for o in DB:
+#    print(o)
+
+
+x = 'John Smith'   #this is working finder !!!! + add into UI as soon as other is done
+for o in DB:
+    if x in o.values():
+        print(o)
+
+
+
+
+
