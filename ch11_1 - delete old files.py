83ecae4ba755b2c051a53d07d7b34d89a46182c6
PyJabba9 <fesenko988@gmail.com>
2025-09-21 15:49:45 +0200

    Ch11 - Final version of search and delete tool - you paste path to folder (or it takes it from clipboard), files sizes to look for and how old are files (in days) then you can choose which ones to delete and which to leave, alternatively delete all

--- ch11_1 - delete old files.py
+++ ch11_1 - delete old files.py
@@ -8,52 +8,113 @@ Print these files with their absolute path to the screen.
 2. logic for storing matches? list? dictionary? how to relate and delete later? - if dictionary - can store path (and call by name to delete if needed)
 key = path; value = list of size and datetime
 
-
-
-
 '''
-import os, datetime, time
+
+def filecleaner(folder,howold,S):   #S - threshold size of the file to look for, howold - how many days old the file has to be to be selected for deletion, folder - path to the folder)
+
+
+
+    folder = Path(folder)
+    L = []
+    FD = {}
+    reply = 0
+    for folder_name, subfolders, filenames in os.walk(folder):
+    #    print(type(folder_name)) # str
+    #    print(type(subfolders)) # list
+    #    print(type(filenames)) # list
+    #    print(type(folder)) # path - works
+
+        L += list(folder.glob('*')) # list of all paths to all files in all directories in the folder
+
+    for files in L:
+        size = os.path.getsize(files)/1024**2
+        DT = datetime.datetime.fromtimestamp(files.stat().st_mtime)
+        #print(files.stat().st_mtime)
+        dt = DT.date()
+        delta = (datetime.datetime.now() - datetime.timedelta(days=howold)).date()
+        #FD.setdefault(files,[int(size),DT.date()])
+        if size >= S and dt <= delta:
+            targetfilesize = size
+            targetfilename = files
+            targetfiledate = DT
+            FD.setdefault(files,[int(size),DT.date()])
+
+
+
+    print(list(FD))
+    print('---------------------------------')
+    deleteall = False
+
+    for key, value in FD.items():
+        print(f'File Path: {key}\nFile Size {value[0]} MB\nFile Last update date: {value[1]}')
+        print('Do you want to delete this file?y/n?\nIf you want to delete everything, type all')
+        r = input()
+        if r == 'y':
+            send2trash.send2trash(Path(key))
+            print('File moved to trash')
+            reply +=1
+        elif r == 'n':
+            print('OK')
+            continue
+        elif r == 'all':
+            deleteall = True
+            break
+            reply +=1
+    try:
+        for key, value in FD.items():
+            if deleteall == True:
+                send2trash.send2trash(Path(key))
+                print(f'File {key} moved to trash')
+                reply +=1
+            else:
+                break
+    except:
+        print(f'Error while deleting {e}') # taken from GPT :) - looks kool I guess
+    if reply == 0:
+        print('File not found')
+
+import os, datetime, time, send2trash, pyperclip
 from pathlib import Path
-folder = 'C:/Users/junke/Downloads'
-folder = Path(folder)
-L = []
-FD = {}
-reply = 0
-for folder_name, subfolders, filenames in os.walk(folder):
-#    print(type(folder_name)) # str
-#    print(type(subfolders)) # list
-#    print(type(filenames)) # list
-#    print(type(folder)) # path - works
+AA = ''
+clip = ''
 
-    L += list(folder.glob('*')) # list of all paths to all files in all directories in the folder
+print('Welcome to my foldercleaner, please provide several values before we start to clean up your folder')
+print('Please provide path to the folder you would like me to analyze and look for old and big files to be deleted. Either paste it manually after typing "type" or I can take it from your clipboard - type "clip"')
+AA = input()
+if AA == 'type':
+    print('Now type file path here:')
+    folder = input()
+    if '\\' in folder:
+        print('please use "/" as a folder path separator, thanks')
+        print('Now type file path here:')
+        folder = input()
+elif AA == 'clip':
+    folder = pyperclip.paste()
+    print(f'Your folder is: {folder}')
+else:
+    print('Wrong input, please type again')
+    folder = input()
+print('Now please enter the size of the file you would like to to search for(in Megabytes - 100 will mean 100MB) - I will look for this size or bigger files')
 
-for files in L:
-    size = os.path.getsize(files)/1024**2
-    DT = datetime.datetime.fromtimestamp(files.stat().st_mtime)
-    #print(files.stat().st_mtime)
-    dt = DT.date()
-    delta = (datetime.datetime.now() - datetime.timedelta(days=9000)).date()
-
-
-    if size >= 100 and dt <= delta:
-        targetfilesize = size
-        targetfilename = files
-        targetfiledate = DT
-        FD.setdefault('File: '+str(targetfilename),['Size: '+str(int(targetfilesize))+' MB','Date of creation: '+str(DT.date())])
-
-
-
-
-
-for key, value in FD.items():
-    if value:
-        print('Files found!')
-        print(f'{key}:{value}')
-        print('Do you want to delete those elements?')
-        print(FD)
-        reply +=1
+while True:
+    try:
+        S = input()
+        S = int(S)
+        break
+    except:
+        print('This has to be a number! Please try again')
 
 
+print('And finally,how old (in days) those files have to be? I will look for this old and older files (e.g 10 os 10 days old)')
+while True:
 
-if reply == 0:
-    print('File not found')
+    try:
+        howold = input()
+        howold = int(howold)
+        break
+    except:
+        print('This has to be a number! Please try again')
+        howold = input()
+
+filecleaner(folder,howold,S)
+
